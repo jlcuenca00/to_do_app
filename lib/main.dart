26ef1fdc71ac5e8b61f3fd99c151b2b63d328c32
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2025-05-05 21:43:48 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
       theme: ThemeData(
-        primaryColor: const Color(0xFF00438A),
+
+        primaryColor: const Color(0xFF00438A), 
         primaryColorLight: const Color(0xFF8DC2E8),
         primaryColorDark: const Color(0xFF003070),
         colorScheme: ColorScheme.fromSwatch().copyWith(
           primary: const Color(0xFF00438A),
           secondary: const Color(0xFFDC1E35),
-          tertiary: const Color(0xFF8DC2E8),
-          surface: Colors.white,
-        ),//ignore
+          tertiary: const Color(0xFF8DC2E8), 
+          background: Colors.white,
+        ),
       ),
     );
   }
